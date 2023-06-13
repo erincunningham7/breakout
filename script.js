a87b41c5c3a783b748bd6994a532db0e12992116
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-13 18:33:53 +0100

    Add user in script.js and style in style.css

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
 
+const userStart = [230, 10]
+let currentPosition = userStart
+
 // Create block
 class Block {
     constructor(xAxis, yAxis) {
@@ -44,3 +47,10 @@ function addBlocks() {
 
 // Call addBlock function
 addBlocks()
+
+// Add user
+const user = document.createElement('div')
+user.classList.add('user')
+user.style.left = currentPosition[0] + 'px'
+user.style.bottom = currentPosition[1] + 'px'
+grid.appendChild(user)
