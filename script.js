00c98680d31b263b277350a6afc8b7e74942f6af
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-13 18:12:47 +0100

    Modify add block function to create for loop in script.js

--- script.js
+++ script.js
@@ -12,14 +12,22 @@ class Block {
     }
 }
 
-// Draw my block function
-function addBlock() {
+// All the blocks
+const blocks = [
+    new Block(10, 270)
+]
+
+// Draw all the blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
     const block = document.createElement('div')
     block.classList.add('block')
     block.style.left = '100px'
     block.style.bottom = '50px'
     grid.appendChild(block)
+    }
+
 }
 
 // Call addBlock function
-addBlock()
+addBlocks()
