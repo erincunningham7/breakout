7b490b4b67d6d2d586b377f6744a72a6b0aeda0e
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-13 18:04:43 +0100

    Create addBlock function and call it in script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const grid = document.querySelector('.grid')
 
-const block = document.createElement('div')
-block.classList.add('block')
-block.style.left = '100px'
-block.style.bottom = '50px'
-grid.appendChild(block)
+// Draw my block function
+function addBlock() {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
+
+// Call addBlock function
+addBlock()
