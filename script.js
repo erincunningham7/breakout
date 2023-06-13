546425a0132d03a47db0563b39f600efd0de1856
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-13 18:15:47 +0100

    Console log test blocks in script.js

--- script.js
+++ script.js
@@ -17,16 +17,17 @@ const blocks = [
     new Block(10, 270)
 ]
 
+console.log(blocks[0])
+
 // Draw all the blocks
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
     const block = document.createElement('div')
     block.classList.add('block')
-    block.style.left = '100px'
+    block.style.left = blocks[i].bottomLeft[0]
     block.style.bottom = '50px'
     grid.appendChild(block)
     }
-
 }
 
 // Call addBlock function
