50ccac89c62c6321471567f1165974e8c607f0f0
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-13 18:08:42 +0100

    Create block class in script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+
+// Create block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis,yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
 
 // Draw my block function
 function addBlock() {
