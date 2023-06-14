66d3aa5424b67f1b622f56260b31712e043e1c8a
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-06-14 21:44:03 +0100

    Create move user function in script.js

--- script.js
+++ script.js
@@ -54,3 +54,12 @@ user.classList.add('user')
 user.style.left = currentPosition[0] + 'px'
 user.style.bottom = currentPosition[1] + 'px'
 grid.appendChild(user)
+
+// Move user
+function moveUser(e) {
+    switch(e.key) {
+        case: 'ArrowLeft':
+            currentPosition[0] -= 10
+            
+    }
+}
